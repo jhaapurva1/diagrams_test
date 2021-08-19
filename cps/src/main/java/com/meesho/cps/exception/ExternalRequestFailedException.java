f0d085ee87aea091dd7eab7864c388115ce8f0a9
Shubham <shubham.aggarwal@meesho.com>
2021-08-19 12:56:16 +0530

    changes

---
+++ cps/src/main/java/com/meesho/cps/exception/ExternalRequestFailedException.java
@@ -0,0 +1,13 @@
+package com.meesho.cps.exception;
+
+/**
+ * @author shubham.aggarwal
+ * 19/08/21
+ */
+public class ExternalRequestFailedException extends Exception {
+
+    public ExternalRequestFailedException(String message) {
+        super(message);
+    }
+
+}
--- cps/src/main/java/com/meesho/cps/helper/CampaignCatalogUpdateEventHelper.java
+++ cps/src/main/java/com/meesho/cps/helper/CampaignCatalogUpdateEventHelper.java
@@ -5,18 +5,18 @@ import com.meesho.ad.client.response.CampaignDetails;
 import com.meesho.cps.data.entity.kafka.CampaignCatalogUpdateEvent;
 import com.meesho.cps.data.entity.mysql.CampaignPerformance;
 import com.meesho.cps.db.mysql.dao.CampaignPerformanceDao;
-
+import com.meesho.cps.exception.ExternalRequestFailedException;
 import com.meesho.cps.service.external.AdService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author shubham.aggarwal
@@ -35,19 +35,27 @@ public class CampaignCatalogUpdateEventHelper {
     @Autowired
     CampaignPerformanceHelper campaignHelper;
 
-    public void onCampaignCatalogUpdate(CampaignCatalogUpdateEvent.CatalogData catalogData) {
+    public void onCampaignCatalogUpdate(CampaignCatalogUpdateEvent.CatalogData catalogData) throws Exception {
         log.info("Received CampaignCatalogUpdateEvent event {}", catalogData);
         Long catalogId = catalogData.getId();
         Long campaignId = catalogData.getCampaignId();
 
+        List<CampaignDetails> campaignDetailsList = adService.getCampaignMetadata(Lists.newArrayList(campaignId));
+        if (CollectionUtils.isEmpty(campaignDetailsList)) {
+            throw new ExternalRequestFailedException("couldn't fetch campaignMetadata from ad service");
+        }
+
+        Map<Long, CampaignDetails> campaignIdAndCampaignDetailsMap = campaignDetailsList.stream()
+                .collect(Collectors.toMap(CampaignDetails::getCampaignId , Function.identity()));
+
         CampaignPerformance campaignPerformance =
                 campaignPerformanceDao.findByCampaignIdAndCatalogId(campaignId, catalogId)
-                        .orElse(CampaignPerformance.builder().campaignId(campaignId).catalogId(catalogId).build());
-
-
-        List<CampaignDetails> campaignDetailsList = adService.getCampaignMetadata(Lists.newArrayList(campaignId));
-        Map<Long, CampaignDetails> campaignIdAndCampaignDetailsMap = campaignDetailsList.stream()
-                        .collect(Collectors.toMap(CampaignDetails::getCampaignId , Function.identity()));
+                        .orElse(CampaignPerformance.builder()
+                                .campaignId(campaignId)
+                                .catalogId(catalogId)
+                                .supplierId(campaignIdAndCampaignDetailsMap.get(campaignId).getSupplierId())
+                                .build()
+                        );
 
         campaignHelper.updateCampaignPerformanceFromHbase(
                 Collections.singletonList(campaignPerformance),
