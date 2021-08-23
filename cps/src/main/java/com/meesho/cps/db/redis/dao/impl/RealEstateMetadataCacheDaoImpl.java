368bbfe9dbb7b5186aa3a643a9fb68d83425d9c1
Shubham <shubham.aggarwal@meesho.com>
2021-08-24 04:10:54 +0530

    revert realestatemetadatacachedaoimpl class

--- cps/src/main/java/com/meesho/cps/db/redis/dao/impl/RealEstateMetadataCacheDaoImpl.java
+++ cps/src/main/java/com/meesho/cps/db/redis/dao/impl/RealEstateMetadataCacheDaoImpl.java
@@ -4,7 +4,7 @@ import com.meesho.commons.enums.Country;
 import com.meesho.cps.data.entity.mysql.RealEstateMetadata;
 import com.meesho.cps.db.mysql.repository.RealEstateMetadataRepository;
 import com.meesho.cps.db.redis.dao.RealEstateMetadataCacheDao;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +12,6 @@ import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author shubham.aggarwal
@@ -26,16 +24,15 @@ public class RealEstateMetadataCacheDaoImpl implements RealEstateMetadataCacheDa
     @Autowired
     RealEstateMetadataRepository realEstateMetadataRepository;
 
-    private volatile Map<Country, Map<String, RealEstateMetadata>> cacheMap = new HashMap<>();
+    private Map<Country,Map<String,RealEstateMetadata>> cacheMap = new HashMap<>();
 
     private Map<Country, ZonedDateTime> lastUpdatedAtMap = new HashMap<>();
 
     @Override
     public void init() {
-        for (Country country : Country.values()) {
-            List<RealEstateMetadata> realEstateMetadataList =
-                    realEstateMetadataRepository.findAllByCountryOrderByUpdatedAtAsc(country);
-            cacheMap.putIfAbsent(country, new HashMap<>());
+        for(Country country : Country.values()){
+            List<RealEstateMetadata> realEstateMetadataList = realEstateMetadataRepository.findAllByCountryOrderByUpdatedAtAsc(country);
+            cacheMap.putIfAbsent(country,new HashMap<>());
             for (RealEstateMetadata realEstateMetadata : realEstateMetadataList) {
                 cacheMap.get(country).put(realEstateMetadata.getName(), realEstateMetadata);
                 lastUpdatedAtMap.put(country, realEstateMetadata.getUpdatedAt());
@@ -53,17 +50,13 @@ public class RealEstateMetadataCacheDaoImpl implements RealEstateMetadataCacheDa
     public long syncUpdatedEntities(Country country) {
         long count = 0;
         ZonedDateTime lastUpdatedAt = lastUpdatedAtMap.get(country);
-        Map<Country, Map<String, RealEstateMetadata>> newMap = new HashMap<>();
-        List<RealEstateMetadata> realEstateMetadataList =
-                realEstateMetadataRepository.findAllByUpdatedAtAfterAndCountryOrderByUpdatedAtAsc(lastUpdatedAt,
-                        country);
-        newMap.putIfAbsent(country, new HashMap<>());
-        for (RealEstateMetadata realEstateMetadata : realEstateMetadataList) {
-            newMap.get(country).put(realEstateMetadata.getName(), realEstateMetadata);
+        List<RealEstateMetadata> realEstateMetadataList = realEstateMetadataRepository.findAllByUpdatedAtAfterAndCountryOrderByUpdatedAtAsc(lastUpdatedAt, country);
+        cacheMap.putIfAbsent(country,new HashMap<>());
+        for (RealEstateMetadata realEstateMetadata : realEstateMetadataList){
+            cacheMap.get(country).put(realEstateMetadata.getName(),realEstateMetadata);
             lastUpdatedAtMap.put(country, realEstateMetadata.getUpdatedAt());
             count++;
         }
-        cacheMap = newMap;
         return count;
     }
 
