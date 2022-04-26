ad1ace66cb8cd5538bd16a27814bd9c7ebf7aa92
anoopkumar-meesho <103181333+anoopkumar-meesho@users.noreply.github.com>
2022-04-26 17:06:45 +0530

    Spads 458 ingestion consumers migration confluent (#41)
    * SPADS-458 dual read ingestion view event and interaction event from confluent kafka for migration
    
    * SPADS-458 removing the default values of confluent username and password
    
    * minor changes
    
    * review change
    
    * review changes
    
    * review changes
    
    * review changes
    
    * review change
    
    Co-authored-by: Rajat Goyal <rajat.goyal@meesho.com>

--- cps/src/main/java/com/meesho/cps/listener/kafka/IngestionViewEventListener.java
+++ cps/src/main/java/com/meesho/cps/listener/kafka/IngestionViewEventListener.java
@@ -1,34 +1,18 @@
 package com.meesho.cps.listener.kafka;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.meesho.ads.lib.constants.Constants;
-import com.meesho.ads.lib.exception.DataValidationException;
-import com.meesho.commons.enums.Country;
 import com.meesho.cps.constants.ConsumerConstants;
-import com.meesho.cps.data.entity.kafka.AdViewEvent;
-import com.meesho.cps.helper.ValidationHelper;
-import com.meesho.cps.service.CatalogViewEventService;
-import com.meesho.cps.service.KafkaService;
 import com.meesho.instrumentation.annotation.DigestLogger;
 import com.meesho.instrumentation.enums.MetricType;
-import com.meesho.instrumentation.metric.statsd.StatsdMetricManager;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
-
-import static com.meesho.cps.constants.TelegrafConstants.*;
 
 /**
  * @author shubham.aggarwal
@@ -39,18 +23,11 @@ import static com.meesho.cps.constants.TelegrafConstants.*;
 public class IngestionViewEventListener {
 
     @Autowired
-    CatalogViewEventService catalogViewEventService;
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
-    private KafkaService kafkaService;
-    @Autowired
-    private StatsdMetricManager statsdMetricManager;
+    IngestionConfluentKafkaViewEventsListener ingestionConfluentKafkaViewEventsListener;
 
     @KafkaListener(id = ConsumerConstants.IngestionViewEventsConsumer.ID, containerFactory =
             ConsumerConstants.IngestionServiceKafka.BATCH_CONTAINER_FACTORY, topics = {
-            "#{'${ingestion.view.event.consumer.topics}'.split(',')}",
-            ConsumerConstants.IngestionViewEventsConsumer.ANONYMOUS_USER_TOPIC}, autoStartup =
+            "#{'${ingestion.view.event.consumer.topics}'.split(',')}"}, autoStartup =
             ConsumerConstants.IngestionViewEventsConsumer.AUTO_START, concurrency =
             ConsumerConstants.IngestionViewEventsConsumer.CONCURRENCY, properties = {
             ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG + "=" +
@@ -58,55 +35,7 @@ public class IngestionViewEventListener {
             ConsumerConfig.MAX_POLL_RECORDS_CONFIG + "=" + ConsumerConstants.IngestionViewEventsConsumer.BATCH_SIZE})
     @DigestLogger(metricType = MetricType.METHOD, tagSet = "consumer=IngestionViewEventListener")
     public void listen(@Payload List<ConsumerRecord<String, GenericRecord>> consumerRecords) {
-
-        List<AdViewEvent> adViewEvents = new ArrayList<>();
-        MDC.put(Constants.GUID, UUID.randomUUID().toString());
-        String countryCode = "IN";
-        MDC.put(Constants.COUNTRY_CODE, Country.getValueDefaultCountryFromEnv(countryCode).getCountryCode());
-
-        for (ConsumerRecord<String, GenericRecord> consumerRecord : consumerRecords) {
-
-            String value = consumerRecord.value().toString();
-            log.info("Ingestion view event received : {}", value);
-
-            AdViewEvent adViewEvent = null;
-            try {
-                adViewEvent = objectMapper.readValue(value, AdViewEvent.class);
-            } catch (JsonProcessingException e) {
-                log.error("JsonProcessingException event : {}", value,e);
-            }
-
-            if (Objects.isNull(adViewEvent) || !ValidationHelper.isValidAdViewEvent(adViewEvent)) {
-                log.error("Invalid event {}", adViewEvent);
-                statsdMetricManager.incrementCounter(VIEW_EVENT_KEY, String.format(VIEW_EVENT_TAGS, NAN, NAN, NAN, INVALID,
-                        NAN));
-                kafkaService.sendMessage(com.meesho.cps.constants.Constants.INGESTION_VIEW_EVENTS_DEAD_QUEUE_TOPIC,
-                        consumerRecord.key(), consumerRecord.value().toString());
-                continue;
-            }
-
-            adViewEvents.add(adViewEvent);
-        }
-
-        try {
-            catalogViewEventService.handle(adViewEvents);
-        } catch (Exception e) {
-            log.error("Exception while processing ingestion view events {}", adViewEvents, e);
-            for (AdViewEvent adViewEvent : adViewEvents) {
-                try {
-                    kafkaService.sendMessage(
-                            com.meesho.cps.constants.Constants.INGESTION_VIEW_EVENTS_DEAD_QUEUE_TOPIC,
-                            String.valueOf(adViewEvent.getProperties().getId()),
-                            objectMapper.writeValueAsString(adViewEvent)
-                    );
-                } catch (JsonProcessingException e1) {
-                    // TODO silent failure needs to be handled
-                    log.error("Failed to push to dead queue event {}", adViewEvent);
-                }
-            }
-        }
-
-        MDC.clear();
+        ingestionConfluentKafkaViewEventsListener.handleIngestionViewEvent(consumerRecords);
     }
 
 }
