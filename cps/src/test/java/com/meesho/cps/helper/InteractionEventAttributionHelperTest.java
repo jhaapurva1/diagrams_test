a22d62e9abeaf6f9db2254ac673507bb219bac69
Navneet <navneet.surana@meesho.com>
2023-04-20 16:05:10 +0530

    minor change

--- cps/src/test/java/com/meesho/cps/helper/InteractionEventAttributionHelperTest.java
+++ cps/src/test/java/com/meesho/cps/helper/InteractionEventAttributionHelperTest.java
@@ -1,16 +1,18 @@
 package com.meesho.cps.helper;
 
 import com.meesho.cps.constants.Constants.CpcData;
+import com.meesho.cps.constants.ConsumerConstants;
 import com.meesho.cps.data.entity.kafka.AdWidgetClickEvent;
 import com.meesho.cps.data.entity.kafka.AdWidgetClickEvent.Properties;
-import java.math.BigDecimal;
-import java.util.HashMap;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RunWith(MockitoJUnitRunner.class)
 public class InteractionEventAttributionHelperTest {
@@ -24,7 +26,7 @@ public class InteractionEventAttributionHelperTest {
         expectedMultipliedCpcData.put(CpcData.MULTIPLIED_CPC, null);
         expectedMultipliedCpcData.put(CpcData.MULTIPLIER, null);
         HashMap<String, BigDecimal> multipliedCpcData = interactionEventAttributionHelper.getMultipliedCpcData(
-            null, null, null);
+                null, null, null);
         Assert.assertEquals(expectedMultipliedCpcData, multipliedCpcData);
     }
 
@@ -34,7 +36,7 @@ public class InteractionEventAttributionHelperTest {
         expectedMultipliedCpcData.put(CpcData.MULTIPLIED_CPC, BigDecimal.TEN);
         expectedMultipliedCpcData.put(CpcData.MULTIPLIER, BigDecimal.ONE);
         HashMap<String, BigDecimal> multipliedCpcData = interactionEventAttributionHelper.getMultipliedCpcData(
-            BigDecimal.TEN, null, null);
+                BigDecimal.TEN, null, null);
         Assert.assertEquals(expectedMultipliedCpcData, multipliedCpcData);
     }
 
@@ -46,10 +48,10 @@ public class InteractionEventAttributionHelperTest {
         expectedMultipliedCpcData.put(CpcData.MULTIPLIER, BigDecimal.TEN);
         WidgetEventHelper widgetEventHelper = new WidgetEventHelper();
         widgetEventHelper.setContext(AdWidgetClickEvent.builder()
-            .properties(Properties.builder().sourceScreen("pdp_reco").build()).build());
+                .properties(Properties.builder().sourceScreen(ConsumerConstants.AdWidgetRealEstates.PDP).build()).build());
         ReflectionTestUtils.setField(widgetEventHelper, "cpcMultiplier", BigDecimal.TEN);
         HashMap<String, BigDecimal> multipliedCpcData = interactionEventAttributionHelper.getMultipliedCpcData(
-            BigDecimal.TEN, null, widgetEventHelper);
+                BigDecimal.TEN, null, widgetEventHelper);
         Assert.assertEquals(expectedMultipliedCpcData, multipliedCpcData);
     }
 }
