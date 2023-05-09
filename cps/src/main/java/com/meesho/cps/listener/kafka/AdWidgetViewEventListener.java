6dc912e9febc762b3fc2555297d53573440f9b90
Navneet <navneet.surana@meesho.com>
2023-05-09 17:56:25 +0530

    added widget view event placeholder tags

--- cps/src/main/java/com/meesho/cps/listener/kafka/AdWidgetViewEventListener.java
+++ cps/src/main/java/com/meesho/cps/listener/kafka/AdWidgetViewEventListener.java
@@ -57,7 +57,7 @@ public class AdWidgetViewEventListener extends BaseManualAcknowledgeKafkaListene
         log.info("Consuming ad widget view event: {}", adWidgetViewEvent);
         if (Boolean.FALSE.equals(AdWidgetValidationHelper.isValidAdWidgetViewEvent(adWidgetViewEvent))) {
             log.error("Invalid event {}", adWidgetViewEvent);
-            telegrafMetricsHelper.increment(WIDGET_VIEW_EVENT_KEY, INTERACTION_EVENT_TAGS, adWidgetViewEvent.getEventName(), NAN, NAN,
+            telegrafMetricsHelper.increment(WIDGET_VIEW_EVENT_KEY, WIDGET_VIEW_EVENT_TAGS, adWidgetViewEvent.getEventName(), NAN, NAN, NAN,
                     AdInteractionStatus.INVALID.name(), NAN);
             return;
         }
